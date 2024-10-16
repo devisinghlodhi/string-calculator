7a828eb0493922bfcbf809e5c2bf582ddef317a6
devisinghlodhi <devisinghlodhi1999@gmail.com>
2024-10-16 17:44:13 +0530

    negative numbers

--- script.js
+++ script.js
@@ -10,16 +10,32 @@ function add(numbers){
     const regex = new RegExp(`[,\n${delimiter}]`);
     const newNumbersArray = numbers.split(regex);
 
-    return newNumbersArray.reduce((acc, value)=>{
+    const negative_numbers = [];
+
+    const total = newNumbersArray.reduce((acc, value)=>{
         if(!isNaN(Number(value))){
             acc += Number(value);
+            if(Number(value) < 0){
+                negative_numbers.push(Number(value))
+            }
         }
         return acc
     }, 0)
+
+    if(negative_numbers.length > 0){
+        throw `negative numbers not allowed ${negative_numbers.join(", ")}`;
+    }
+
+    return total;
 }
 
 
+try {
+    const result = add("//;\n1,2,3,4\n5,6;7,30")
+    console.log(result)
+} catch (error) {
+    console.log(error)
+}
 
-const result = add("//;\n1,2,3,4\n5,-6;7")
-console.log(result)
+
 
